597d4471aafd78b10b1edd6cfc865627b6709c32
JethroNatividad <natividad.jet@gmail.com>
2024-01-24 11:28:06 +0800

    implement get_input function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::Write;
+
 // Write a program that takes in rate of return and outputs the estimate years on how long it will take to double your investment.
 // Inputs: rate of return
 // Process: 72/rate of return
@@ -20,6 +23,23 @@ mod tests {
         assert_eq!(get_return_investment_years(4.0), Ok(18.0));
         assert_eq!(get_return_investment_years(10.0), Ok(7.2));
         assert_eq!(get_return_investment_years(0.0), Err("Invalid Rate"));
+    }
+}
+
+fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        match input.trim().parse() {
+            Ok(value) => break value,
+            Err(_) => println!("Invalid input. Please try again."),
+        }
     }
 }
 
