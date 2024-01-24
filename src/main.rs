7caacb609f562581dd0918fee7faae008662beda
JethroNatividad <natividad.jet@gmail.com>
2024-01-24 11:22:48 +0800

    implement get return investment function

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,23 @@
 // Process: 72/rate of return
 // Output: It will take n years to double your initial investment.
 
+fn get_return_investment_years(rate: f64) -> Result<f64, &'static str> {
+    if rate < 1.0 {
+        return Err("Invalid Rate");
+    }
+    let years: f64 = 72.0 / rate;
+    Ok(years)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_get_return_investment_years() {
-        assert_eq!(get_return_investment_years(4.0), 18.0);
-        assert_eq!(get_return_investment_years(10.0), 7.2);
-
-        #[should_panic]
-        get_return_investment_years(0.0);
+        assert_eq!(get_return_investment_years(4.0), Ok(18.0));
+        assert_eq!(get_return_investment_years(10.0), Ok(7.2));
+        assert_eq!(get_return_investment_years(0.0), Err("Invalid Rate"));
     }
 }
 
